e180873733fae1ab3cd8323e9ead9c0d1acc703d
Naimesh Barot <naimeshrao@gmail.com>
2026-02-09 13:51:13 +0530

    added default vscode settings

--- src/components/Antd/Button/Button.style.ts
+++ src/components/Antd/Button/Button.style.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 import { Button as AntdButton } from 'antd'
 
-export const StyledButton = styled(AntdButton)<{
-  btnType?: 'primary' | 'secondary' | 'danger'
-}>`
+type BtnType = 'primary' | 'secondary' | 'danger'
+
+interface StyledButtonProps {
+  btnType?: BtnType
+}
+
+export const StyledButton = styled(AntdButton)<StyledButtonProps>`
   &.ant-btn {
     display: flex;
     align-items: center;
     justify-content: center;
     font-weight: 600;
-    background-color: ${({ theme }) => theme.colors.primary};  
+    background-color: ${({ theme }) => theme.colors.primary};
+  }
 `
